33241688a0b3d4d67c9e3a38d2cc53efec5f4f79
Hans Fluhmann <hfluhmann@gmail.com>
2024-06-11 02:14:41 -0400

    added cors

--- backend/index.js
+++ backend/index.js
@@ -2,10 +2,12 @@ require('dotenv').config()
 const express = require('express');
 const app = express();
 const port = parseInt(process.env.PORT);
-var bodyParser = require('body-parser')
+const bodyParser = require('body-parser')
 const { sequelize } = require('./connection');
 const { Post } = require('./models/posts');
+const cors = require("cors");
 
+app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
